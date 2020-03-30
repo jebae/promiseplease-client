4697955e2e8a1c33dc5b8202cf1d544ae72e298b
bjm07 <bjmm9301@gmail.com>
2020-03-30 14:32:03 +0900

    modify title in homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,7 +18,7 @@ import Helmet from "react-helmet";
 const FETCH_COUNT = 11;
 
 const head = {
-	title: "약속해줘 - 당신의 공약을 보여주세요",
+	title: "약속해줘 - 21대 총선 투표소와 후보 공약",
 	description: "21대 국회의원 선거 투표소와 후보들의 공약을 찾아보세요",
 	keywords: [ "총선", "21대 총선", "투표", "국회의원 선거", "선거", "투표소", "공약", ],
 	image: "https://www.dropbox.com/s/vbq4p10qaw4fz0e/vote.jpg?raw=1",
